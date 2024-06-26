8de7bc11ea0f244038b3c111507fa9cd3a9a3533
Marlon Kuzmick <mkuzmick@gmail.com>
2024-06-26 12:44:40 -0400

    Updates:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Dani from "@/components/line-art/ll-people/Dani";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-3/4">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-5xl font-black p-8">mk paths week</h1>
+      <div className="w-1/2">
         <Dani />
       </div>
       
